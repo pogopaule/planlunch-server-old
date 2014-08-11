7b2c3bcae3de54c9b899ad03bc48baf2cd41e57b
pogopaule <pogopaule@gmx.de>
2014-08-11 20:41:31 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var Hapi = require('hapi'),
+    _ = require('underscore'),
+    Joi = require('joi');
+
+var server = new Hapi.Server(8080, {cors: true});
+
+server.route({
+  method: 'GET',
+  path: '/places',
+  handler: function(request, reply) {
+    reply(server.places);
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/places/{name}',
+  config: {
+    validate: {
+      payload: Joi.object().keys({
+        attendee: Joi.string().required()
+      })
+    }
+  },
+  handler: function(request, reply) {
+    var attendee = request.payload.attendee;
+    removeAttendeeFromPlaces(attendee);
+    addAttendeeToPlace(attendee, request.params.name);
+
+    reply();
+  }
+});
+
+if(!isTest()) {
+  server.start(function() {
+    console.log('server started', server.info.uri);
+  });
+}
+
+
+
+
+
+
+function isTest() {
+  return module.parent;
+}
+
+function removeAttendeeFromPlaces(attendee) {
+  var placeAttendedByAttendee = _.each(server.places, function(place) {
+    if(place.attendees) {
+      place.attendees = _.without(place.attendees, attendee);
+    }
+  });
+}
+
+function addAttendeeToPlace(attendee, placeName) {
+  var place = _.find(server.places, function(place) {
+    return place.name === placeName
+  });
+
+  if(!place.attendees) {
+    place.attendees = [];
+  }
+
+  place.attendees.push(attendee);
+}
+
+server.places = [
+  { name: 'Anuras Elefant', geo: [47.9949945, 7.8373675] },
+  { name: 'Café Einstein', geo: [47.9986877, 7.8396364] },
+  { name: 'Curryhaus', geo: [47.9995368, 7.8386085] },
+  { name: 'Lila Bar', geo: [47.9939093, 7.8419898] },
+  { name: 'Amara Schlemmer Stüble', geo: [47.9968696, 7.8351075] },
+  { name: 'Café Huber', geo: [47.9973051, 7.839442] },
+  { name: 'Brasil', geo: [47.9982693, 7.8362522] },
+  { name: 'mensadrei', geo: [47.994701, 7.848078] },
+  { name: 'Euphrat', geo: [47.994794, 7.8479557] },
+  { name: 'Leaf Thaiküche', geo: [47.9975493, 7.8445847] },
+  { name: 'Jos Fritz Café', geo: [47.9944926, 7.8415117] },
+  { name: 'Café Sedan', geo: [47.9950339, 7.8430912] },
+  { name: 'Inxmail Küche', geo: [47.9956715, 7.8386571] },
+  { name: 'Der Geier', geo: [47.9940068, 7.8412263] },
+  { name: 'Inxmail Dachterrase', geo: [47.99518, 7.83837] },
+  { name: 'Mai Sushi', geo: [47.9959956, 7.8440229] },
+  { name: 'Mai Wok', geo: [47.9960166, 7.8439047] },
+  { name: 'Stühlingerpark', geo: [47.99666, 7.83839] },
+  { name: 'Kartoffelhaus', geo: [47.9880838, 7.8463505] },
+  { name: 'la Centrale', geo: [47.9937667, 7.8336526] },
+  { name: 'Corosol', geo: [47.9947838, 7.8346218] },
+  { name: 'Pizzaria Ochsebrugg', geo: [47.9979355, 7.8402287] }
+];
+
+module.exports = server;
---
+++ test/unit.js
@@ -0,0 +1,87 @@
+var Lab = require('lab'),
+    Joi = require('joi'),
+    _ = require('underscore'),
+    lab = exports.lab = Lab.script(),
+    server = require('../index.js'),
+    places = server.places;
+
+
+
+
+lab.experiment('Places endpoint', function() {
+
+  lab.test('lists all places', function(done) {
+    var options = {
+      method: 'GET',
+      url: '/places'
+    };
+
+    var placeSchema = Joi.object().keys({
+      name: Joi.string().required(),
+      geo: Joi.array().includes(Joi.number().min(0).max(180)).length(2).required(),
+      attendees: Joi.array().includes(Joi.string())
+    }),
+    resultSchema = Joi.array().includes(placeSchema).min(1);
+
+    server.inject(options, function(response) {
+      var result = response.result;
+
+      Lab.expect(response.statusCode).to.equal(200);
+      Joi.assert(result, resultSchema);
+
+      done();
+    });
+  });
+
+  lab.test('can add attendee to place', function(done) {
+    var options = {
+      method: 'POST',
+      url: '/places/Café%20Einstein',
+      payload: {attendee: 'Max'}
+    }
+
+    server.inject(options, function(response) {
+      Lab.expect(response.statusCode).to.equal(200);
+      Lab.expect(places[1].attendees).to.contain('Max');
+
+      done();
+    });
+  });
+
+  lab.test('allows attendee to change place', function(done) {
+    var options = {
+      method: 'POST',
+      url: '/places/Café%20Einstein',
+      payload: {attendee: 'Max'}
+    }
+
+    places[2].attendees = ['Max'];
+
+    server.inject(options, function(response) {
+      var placesAttendedByMax = _.filter(places, function(place) {
+        if(place.attendees) {
+          return _.contains(place.attendees, 'Max');
+        }
+        return false;
+      });
+      Lab.expect(response.statusCode).to.equal(200);
+      Lab.expect(places[1].attendees).to.contain('Max');
+      Lab.expect(placesAttendedByMax).length.to.be(1);
+
+      done();
+    });
+  });
+
+  lab.test('rejects POST requests with invalid payload format', function(done) {
+    var nonStringAttendee = {
+      method: 'POST',
+      url: '/places/Café%20Einstein',
+      payload: {attendee: {a: 'foo'}}
+    }
+    server.inject(nonStringAttendee, function(response) {
+      Lab.expect(response.statusCode).to.equal(400);
+
+      done()
+    });
+  })
+});
