185ec5f3d725a20cb3ab8e352761d79a6175996e
pogopaule <pogopaule@gmx.de>
2014-09-10 21:54:54 +0200

    set cronjob to correct time

--- cronjobs.js
+++ cronjobs.js
@@ -1,7 +1,7 @@
 var CronJob = require('cron').CronJob;
 
 module.exports = function(job) {
-  return new CronJob('* * * * * 1-5', function(){
+  return new CronJob('0 0 14 * * 1-5', function(){
     job();
   }, null, true, 'Europe/Berlin');
 }
